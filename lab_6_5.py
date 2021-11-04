3d649744f9e3ac9b34d492e0a0c40521411f68a5
dimitriyevchinho22 <dimitrii.yevchenko@ucu.edu.ua>
2021-11-04 23:24:20 +0200

    version2

--- lab_6_5.py
+++ lab_6_5.py
@@ -20,18 +20,3 @@ def generate_grid() -> List[List[str]]:
 from typing import List
 
 
-def generate_grid() -> List[List[str]]:
-    """
-    Generates list of lists of letters - i.e. grid for the game.
-    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
-    """
-    import random
-    from string import ascii_uppercase
-    final_list = []
-    for i in range(3):
-        small_list = []
-        for j in range(3):
-            j = random.choice(ascii_uppercase)
-            small_list.append(j)
-        final_list.append(small_list)
-    return final_list
